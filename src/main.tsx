152a678e5604e178a3c481ff1c3c845c28ebae13
trunglvb <levubaotrung0311@gmail.com>
2024-03-21 20:40:44 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import App from './App'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
